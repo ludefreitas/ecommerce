964cc7f47292ae0c57d517c523be0829138497d0
Luciano Freitas <lulufreitas08@hotmail.com>
2020-09-21 23:01:46 -0300

    inicio do projeto

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+
+echo "Graças a Deus !!!!";
+
+
+
+/*
+require_once("vendor/autoload.php");
+
+$app = new \Slim\Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function() {
+    
+	echo "OK";
+
+});
+
+$app->run();
+
+ ?>
+*/
