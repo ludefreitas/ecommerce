e8b35199d120cac3d5b3f7fd813692039998d17e
Luciano Freitas <lulufreitas08@hotmail.com>
2020-09-23 01:40:41 -0300

    Adicionaddo classe PageAdmin

--- index.php
+++ index.php
@@ -2,17 +2,31 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Luciano\Page;
+use \Luciano\PageAdmin;
+
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$page = new Luciano\Page();
+	$page = new Page();
 
 	$page->setTpl("index");
 
 });
+
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
+
+	$page->setTpl("index");
+
+});
+
 
 $app->run();
 
