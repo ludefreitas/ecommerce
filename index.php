09d7e13ac1c75cda75994e607d46824ba60fd188
Luciano Freitas <lulufreitas08@hotmail.com>
2020-09-26 11:38:18 -0300

    Admin template

--- index.php
+++ index.php
@@ -61,6 +61,97 @@ $app->get('/admin/logout', function(){
 
 });
 
+// Rota para listar todos usuários da classe 
+$app->get('/admin/users', function() {
+
+	User::verifyLogin();
+	// na linha abaixo retorna um array com todos os dados do usuário
+	$users = User::listAll();
+	// carrega uma pagina das páginas do admin
+	$page = new PageAdmin();
+
+	// envia para a página o array retornado pelo listAll
+	$page->setTpl("users", array( // aqui temos um array com muitos arrays
+		"users"=>$users
+	));
+});
+
+
+$app->get('/admin/users/create', function() {
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-create");
+});
+
+$app->get('/admin/users/:iduser/delete', function($iduser) {
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header("Location: /admin/users");
+	exit();
+});
+
+
+$app->get('/admin/users/:iduser', function($iduser) {
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-update", array(
+		"user"=>$user->getValues()
+	));
+});
+
+$app->post('/admin/users/create', function() {
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	//$_POST['despassword'] = User::getPasswordHash($_POST['despassword']);
+
+	$user->setData($_POST);
+
+	$user->save();
+
+	header("Location: /admin/users");
+	exit();
+});
+
+$app->post('/admin/users/:iduser', function($iduser) {
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+	
+	$user->get((int)$iduser);
+
+	$user->setData($_POST);
+	
+	$user->update();
+
+	header("Location: /admin/users");
+	exit();
+});
+
 $app->run();
 
  ?>
--- vendor/ludefreitas/php-classes/src/Model/User.php
+++ vendor/ludefreitas/php-classes/src/Model/User.php
@@ -63,7 +63,86 @@ class User extends Model {
 		$_SESSION[User::SESSION] = NULL;
 	}
 
-}
+public static function listAll()
+	{
 
+		$sql = new Sql();
+
+		return $sql->select("
+			SELECT * FROM tb_users a 
+			INNER JOIN tb_persons b 
+			using(idperson) 
+			ORDER BY b.desperson");
+	}
+	
+	public function save()
+	{
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+			":desperson"=>utf8_decode($this->getdesperson()),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>User::getPasswordHash($this->getdespassword()),
+			":desemail"=>$this->getdesemail(),
+			":nrphone"=>$this->getnrphone(),
+			":inadmin"=>$this->getinadmin()		
+		));
+
+		//var_dump($results);
+		//exit();
+
+		$this->setData($results[0]);
+	}
+
+	public function get($iduser)
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser", array(
+			":iduser"=>$iduser
+		));
+
+		$this->setData($results[0]);
+		
+	}
+
+	public function update()
+	{
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+			":iduser"=>$this->getiduser(),
+			":desperson"=>utf8_decode($this->getdesperson()),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>$this->getdespassword(),
+			//":despassword"=>User::getPasswordHash($this->getdespassword()),
+			":desemail"=>$this->getdesemail(),
+			":nrphone"=>$this->getnrphone(),
+			":inadmin"=>$this->getinadmin()
+		));
+
+		$this->setData($results[0]);
+	}
+
+	public function delete()
+	{
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_users_delete(:iduser)", array(
+			":iduser"=>$this->getiduser()
+		));
+	}
+
+	public static function getPasswordHash($password)
+	{
+
+		return password_hash($password, PASSWORD_DEFAULT, [
+			'cost'=>12
+		]);
+
+	}
+
+}
 
 ?>
